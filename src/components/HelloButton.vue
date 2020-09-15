1312e2d606107799266207b4816ca700259fb81e
lqwangxg <lqwangxg@gmail.com>
2020-09-15 05:05:09 +0000

    commit

--- src/components/HelloButton.vue
+++ src/components/HelloButton.vue
@@ -1,23 +1,27 @@
 <template>
-    <button>Hello Button</button>
+    <button @click="onClick">Hello Button</button>
 </template>
 
 <script lang="ts">
-    import {Component, Emit, Prop, Vue} from "vue-property-decorator";
+    import {Options, Vue} from "vue-class-component";
+
+    @Options({
+      props: {
+        greet: String
+      }
+    })
 
     @Component
     export default class HelloButton extends Vue{
-      @Prop()
-      public greet?: string;
-
-      @Emit()
+      
       public click(){
-        console.log("HelloButton clicked on Emit. ");
+        this.$emit("click", this.greet);
+        console.log("HelloButton clicked from parent to child by $emit. ");
       }
 
-      public onclick(){
+      public onClick(){
         alert(this.greet);
-        this.greet = "こんにちは";
+        this.greet = "こんにちは, from HelloButton Self.";
       }
     }
 </script>
