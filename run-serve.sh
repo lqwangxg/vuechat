36123186f80b30aecfaa20b8b20201a36b2e1de5
lqwangxg <lqwangxg@gmail.com>
2020-09-15 04:28:37 +0000

    commit

---
+++ run-serve.sh
@@ -0,0 +1,22 @@
+builder_name="lqwangxg/vue-cli"
+builder_container_name=vue-dev
+vuecli=`docker images | grep "$builder_name"`
+if [ $? = 1 ]; then
+  echo "docker image[$builder_name] is not found. start creating ..."
+  docker build -t lqwangxg/vue-cli -f Dockerfile.builder .
+else
+  echo "found image [$builder_name]."
+fi
+
+cid=`docker ps -a | grep $builder_container_name | awk '{print $1}'`
+if [ -z "$cid" ]; then 
+  echo "docker ps [$builder_container_name] is not found, run...";
+  docker run -it --name $builder_container_name \
+  -v ~/vuechat:/vuechat \
+  -dp 8080:8080 \
+  $builder_name \
+  npm run serve
+else 
+  echo "docker ps [$builder_container_name] is found, start ";
+  docker start [$builder_cotainer_name]
+fi
