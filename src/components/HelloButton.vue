1305fec411ff976ffa387029138aaa3cd8664ad0
lqwangxg <lqwangxg@gmail.com>
2020-09-15 04:45:53 +0000

    commit

---
+++ src/components/HelloButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <button>Hello Button</button>
+</template>
+
+<script lang="ts">
+    import {Component, Emit, Prop, Vue} from "vue-property-decorator";
+
+    @Component
+    export default class HelloButton extends Vue{
+      @Prop()
+      public greet?: string;
+
+      @Emit()
+      public click(){
+        console.log("HelloButton clicked on Emit. ");
+      }
+
+      public onclick(){
+        alert(this.greet);
+        this.greet = "こんにちは";
+      }
+    }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,25 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloButton :greet="greetText" @click="onHelloButtonClicked" ></HelloButton>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'; // @is alias to /src
+import HelloButton from '@/components/HelloButton.vue'; 
 
 @Options({
   components: {
     HelloWorld,
+    HelloButton
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  public greetText: string = "hello";
+  public onHelloButtonClicked(){
+    this.greetText = "こんにちは、よろしくお願いいたします。";
+  }
+}
 </script>
