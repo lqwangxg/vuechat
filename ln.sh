5eefa4096b700a894e5c8a2d7df4700315c16336
lqwangxg <lqwangxg@gmail.com>
2020-09-16 08:46:28 +0000

    commit

--- ln.sh
+++ ln.sh
@@ -1,7 +1,9 @@
-from=~/vuechat
-target=.
+from="/root/vuechat"
+target="."
 ##=======================================
-ln -f /build.sh  /build.sh
-ln -f /edit.sh /edit.sh
-ln -f /run-serve.sh /run-serve.sh
-ln -f /gitpush.sh /gitpush.sh
+ln -f ${target}/build.sh ${from}/build.sh
+ln -f ${target}/edit.sh ${from}/edit.sh
+ln -f ${target}/run-serve.sh ${from}/run-serve.sh
+ln -f ${target}/gitpush.sh ${from}/gitpush.sh
+ln -f ${target}/ln.sh ${from}/ln.sh
+ln -f ${target}/Dockerfile.builder ${from}/Dockerfile.builder
